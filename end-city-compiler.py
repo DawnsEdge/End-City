8aec5861ab8590ac55afdb28e819b73ee7e5e536
Ben Petrie <thebenpetrie@gmail.com>
2019-05-01 10:55:06 -0600

    Added more to Lang.json and made it actually work

--- end-city-compiler.py
+++ end-city-compiler.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def save(d, filename):
    file = open(filename,'w')
@@ -10,6 +11,20 @@ def load(filename):
    d = json.load(file)
    file.close()
    return d
+
+def decode(split, part):
+   ret = part
+   spart = ["", "", ""]
+   spart2 = ["", "", ""]
+   try:
+      spart = part.split("{")
+      if len(spart) > 1:
+         spart2 = spart[1].split("}")
+         if len(split)-1 >= int(spart2[0])+1:
+            ret = split[int(spart2[0])+1]
+   except KeyError:
+      return "!!--==ERROR==--!!"
+   return ret + spart2[1]
 
 noerror = False
 while noerror == False:
@@ -25,20 +40,45 @@ lang = load("lang.json")
 lines = f.read().splitlines()
 i = 1
 Compile = True
+mcf = []
 for line in lines:
-    split = line.split(" ")
-    if split[0] in lang["commands"]:
-        print line
-    else:
-        try:
-            if lang["code"][split[0]] != None:
-                if len(split) < lang["code"][split[0]]["parameters"]:
-                    print "[ERROR]", split[0], "takes at least", str(lang["code"][split[0]]["requiredparameters"]), "at most", str(lang["code"][split[0]]["parameters"])
-                #print lang["code"][split[0]]["cmd"]
-        except KeyError:
-            print "[ERROR] Unknown \"" + split[0] + "\" on line", str(i)
-            Compile = False
-    i += 1
+   split = line.split(" ")
+   if split[0] in lang["commands"]:
+     mcf.append(line)
+   else:
+      try:
+         if lang["code"][split[0]] != None:
+            if len(split)-1 < lang["code"][split[0]]["requiredarguments"]:
+               print "[ERROR]", split[0], "takes at least", str(lang["code"][split[0]]["requiredarguments"]), "arguments and at most", str(lang["code"][split[0]]["arguments"]), "arguments"
+            else:
+               cmd = lang["code"][split[0]]["cmd"]
+               splitcmd = cmd.split(" ")
+               newcmd = ""
+               for part in splitcmd:
+                  add = part
+                  spart = part.split("|")
+                  if len(spart) > 1:
+                     d1 = decode(split, spart[0])
+                     d2 = decode(split, spart[1])
+                     if d1[0] == "{":
+                        add = d2
+                     else:
+                        add = d1
+                  else:
+                     add = decode(split, part)
+                  newcmd += add + " "
+               mcf.append(newcmd)
+      except KeyError:
+         print "[ERROR] Unknown \"" + split[0] + "\" on line", str(i)
+         Compile = False
+   i += 1
 
 if Compile == False:
-    print "Unable to Compile. Fix all errors."
+   print "Unable to Compile. Fix all errors."
+if Compile == True:
+   os.mkdir("compiled")
+   newfile = open("compiled/" + filename + ".mcfunction", "w+")
+   for line in mcf:
+      newfile.write(line + "\n")
+   newfile.close()
+   print "Compiled!"
